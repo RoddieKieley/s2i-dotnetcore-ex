50a2121a018061c912cd807d527b39c8667a0d7c
Roddie Kieley <rkieley@unifiedsoftworx.com>
2018-05-02 19:49:58 -0230

    Stripped Main to basics.

--- app/Program.cs
+++ app/Program.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+//using Microsoft.AspNetCore;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+using System.Threading;
 
 namespace app
 {
@@ -14,12 +15,17 @@ namespace app
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            //BuildWebHost(args).Run();
+            while (true)
+            {
+                Console.WriteLine("Hello OpenShift World!");
+                Thread.Sleep(10000);
+            }
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        //public static IWebHost BuildWebHost(string[] args) =>
+            //WebHost.CreateDefaultBuilder(args)
+                //.UseStartup<Startup>()
+                //.Build();
     }
 }
