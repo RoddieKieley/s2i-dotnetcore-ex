a66696f35917566da23af12048fe504724102c3b
Roddie Kieley <rkieley@unifiedsoftworx.com>
2018-05-02 21:36:07 -0230

    Updated project and program from HelloWorld-simple.

--- app/Program.cs
+++ app/Program.cs
@@ -1,31 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-//using Microsoft.AspNetCore;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.Logging;
-using System.Threading;
+﻿//  ------------------------------------------------------------------------------------
+//  Copyright (c) 2015 Red Hat, Inc.
+//  All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the ""License""); you may not use this
+//  file except in compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+//  EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR
+//  CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR
+//  NON-INFRINGEMENT.
+//
+//  See the Apache Version 2.0 License for specific language governing permissions and
+//  limitations under the License.
+//  ------------------------------------------------------------------------------------
 
-namespace app
+//
+// HelloWorld_simple
+//
+// Command line:
+//   HelloWorld_simple [brokerUrl [brokerEndpointAddress]]
+//
+// Default:
+//   HelloWorld_simple amqp://localhost:5672 amq.topic
+//
+// Requires:
+//   An unauthenticated broker or peer at the brokerUrl 
+//   capable of receiving and delivering messages through 
+//   the endpoint address.
+//
+using System;
+using Amqp;
+
+namespace HelloWorld_simple
 {
-    public class Program
+    class HelloWorld_simple
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            //BuildWebHost(args).Run();
-            while (true)
-            {
-                Console.WriteLine("Hello OpenShift World!");
-                Thread.Sleep(10000);
-            }
-        }
+            string broker  = args.Length >= 1 ? args[0] : "amqp://broker-app-amq-amqp.amq71-basic-1.svc:5672";
+            string address = args.Length >= 2 ? args[1] : "amq.topic";
 
-        //public static IWebHost BuildWebHost(string[] args) =>
-            //WebHost.CreateDefaultBuilder(args)
-                //.UseStartup<Startup>()
-                //.Build();
+            Address brokerAddr = new Address(broker);
+            Connection connection = new Connection(brokerAddr);
+            Session session = new Session(connection);
+
+            SenderLink sender = new SenderLink(session, "helloworld-sender", address);
+            ReceiverLink receiver = new ReceiverLink(session, "helloworld-receiver", address);
+
+            Message helloOut = new Message("Hello World!");
+            sender.Send(helloOut);
+
+            Message helloIn = receiver.Receive();
+            receiver.Accept(helloIn);
+
+            Console.WriteLine(helloIn.Body.ToString());
+
+            receiver.Close();
+            sender.Close();
+            session.Close();
+            connection.Close();
+        }
     }
 }
